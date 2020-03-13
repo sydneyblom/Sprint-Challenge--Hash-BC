236082d5c179430eac1488c6da8493b63e1203ad
Sydney Blom <sydneyblom@#gmail.com>
2020-03-13 11:17:39 -0500

    Mining on server started proof at - 3000000

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,13 +1,9 @@
 import hashlib
 import requests
-
 import sys
-
 from uuid import uuid4
-
 from timeit import default_timer as timer
-
-import random
+from random import randint
 
 
 def proof_of_work(last_proof):
@@ -19,50 +15,51 @@ def proof_of_work(last_proof):
     - p is the previous proof, and p' is the new proof
     - Use the same method to generate SHA-256 hashes as the examples in class
     """
-
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    proof = -30000000
     #  TODO: Your code here
 
+    # while valid_proof(last_hash, proof) is False:
+    #     proof += 1
+    while valid_proof(last_proof, proof) is False:
+        proof -= 1
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
-
-
 def valid_proof(last_hash, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the hash of the last proof match the first six characters of the hash
     of the new proof?
-
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
-
     # TODO: Your code here!
-        # find last hash proof (slice -6)
-    # encode proof
+    # encode last proof
+    last_proof = f"{last_hash}".encode()
     # use sha256
-    # if sha256 == last hash proof
-        # return TRUE
-    # else return FALSE
-
-
+    last_hash = hashlib.sha256(last_proof).hexdigest()
+    # encode guess proof
+    guess = f"{proof}".encode()
+    # use sha256
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # last hash (slice -6)
+    # guess hash (slice 6)
+    # return True of False
+    return guess_hash[:6] == last_hash[-6:]
 if __name__ == '__main__':
     # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
-        node = "https://lambda-coin-test-1.herokuapp.com/api"
-
+        node = "https://lambda-coin.herokuapp.com/api"
+        # node = "https://lambda-coin-test-1.herokuapp.com/api"
     coins_mined = 0
-
     # Load or create ID
     f = open("my_id.txt", "r")
     id = f.read()
     print("ID is", id)
     f.close()
-
     if id == 'NONAME\n':
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
@@ -72,10 +69,8 @@ if __name__ == '__main__':
         r = requests.get(url=node + "/last_proof")
         data = r.json()
         new_proof = proof_of_work(data.get('proof'))
-
         post_data = {"proof": new_proof,
                      "id": id}
-
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
         if data.get('message') == 'New Block Forged':
